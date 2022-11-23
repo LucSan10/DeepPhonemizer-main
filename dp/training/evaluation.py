e2f4ef7bb108a6e834cb1a3e8488af5ce02bac46
LucSan10 <santiagolucas643@gmail.com>
2022-11-23 17:27:42 -0300

    conserta erro de coleta de estatística

--- dp/training/evaluation.py
+++ dp/training/evaluation.py
@@ -2,6 +2,30 @@ from typing import List, Tuple, Dict, Any
 
 from dp.training.metrics import *
 
+def collect_evaluation_results(eval_result, languages, lang_edits, lang_counts, lang_errors, type: str):
+    edits, sizes, errors, counts = [], [], [], []
+    for lang in languages:
+        edit = sum(lang_edits[lang].values())
+        edits.append(edit)
+
+        size = sum(lang_counts[lang].values())
+        sizes.append(size)
+        
+        error = sum(lang_errors[lang].values())
+        errors.append(error)
+        
+        count = len(lang_errors[lang])
+        counts.append(count)
+
+        edits_rate = edit / size
+        error_rate = error / count
+
+        eval_result.setdefault(lang, {}).update({f'{type}_edits_rate': edits_rate})
+        eval_result.setdefault(lang, {}).update({f'{type}_error_rate': error_rate})
+
+    eval_result[f'mean_{type}_edits_rate'] = sum(edits) / sum(sizes)
+    eval_result[f'mean_{type}_error_rate'] = sum(errors) / sum(counts)
+        
 
 def evaluate_samples(lang_samples: Dict[str, List[Tuple[List[str], List[str], List[str]]]]) -> Dict[str, Any]:
     """Calculates word and phoneme error rates per language and their mean across languages
@@ -17,117 +41,88 @@ def evaluate_samples(lang_samples: Dict[str, List[Tuple[List[str], List[str], Li
     """
 
     evaluation_result = dict()
+    
     lang_trans_edd, lang_trans_count, lang_trans_err = dict(), dict(), dict()
     lang_phon_edd, lang_phon_count, lang_phon_err = dict(), dict(), dict()
     lang_syll_edd, lang_syll_count, lang_syll_err = dict(), dict(), dict()
+    lang_stress_edd, lang_stress_count, lang_stress_err = dict(), dict(), dict()
+    lang_pause_edd, lang_pause_count, lang_pause_err = dict(), dict(), dict()
+
     languages = sorted(lang_samples.keys())
     for lang in languages:
         for word, generated, target in lang_samples[lang]:
             word = ''.join(word)
             generated = ''.join(str(s) for s in generated)
             target = ''.join(str(s) for s in target)
-
-            trans_edd, trans_count = transcription_edit_distance(generated, target)
-            trans_err = transcription_error(generated, target)
-            
-            phon_edd, phon_count = phoneme_edit_distance(generated, target)
-            phon_err = phoneme_error(generated, target)
-            
-            syll_edd, syll_count = syllable_edit_distance(generated, target)
-            syll_err = syllable_error(generated, target)
-
+# ----------------------------------------------------------------------------------------------------------------------------------------------------------
             trans_edd_dict = lang_trans_edd.setdefault(lang, dict())
             trans_count_dict = lang_trans_count.setdefault(lang, dict())
             trans_err_dict = lang_trans_err.setdefault(lang, dict())
-            
+# ----------------------------------------------------------------------------------------------------------------------------------------------------------
             phon_edd_dict = lang_phon_edd.setdefault(lang, dict())
             phon_count_dict = lang_phon_count.setdefault(lang, dict())
             phon_err_dict = lang_phon_err.setdefault(lang, dict())
-            
+# ----------------------------------------------------------------------------------------------------------------------------------------------------------
             syll_edd_dict = lang_syll_edd.setdefault(lang, dict())
             syll_count_dict = lang_syll_count.setdefault(lang, dict())
             syll_err_dict = lang_syll_err.setdefault(lang, dict())
-            
+# ----------------------------------------------------------------------------------------------------------------------------------------------------------
+            stress_edd_dict = lang_stress_edd.setdefault(lang, dict())
+            stress_count_dict = lang_stress_count.setdefault(lang, dict())
+            stress_err_dict = lang_stress_err.setdefault(lang, dict())
+# ----------------------------------------------------------------------------------------------------------------------------------------------------------
+            pause_edd_dict = lang_pause_edd.setdefault(lang, dict())
+            pause_count_dict = lang_pause_count.setdefault(lang, dict())
+            pause_err_dict = lang_pause_err.setdefault(lang, dict())
+# ----------------------------------------------------------------------------------------------------------------------------------------------------------
+            trans_edd, trans_count = transcription_edit_distance(generated, target)
+            trans_err = transcription_error(generated, target)
+
             best_trans_err, best_trans_count = trans_edd_dict.get(word, None), trans_count_dict.get(word, None)
             if best_trans_err is None or trans_edd / trans_count < best_trans_err / best_trans_count:
                 trans_edd_dict[word] = trans_edd
                 trans_count_dict[word] = trans_count
                 trans_err_dict[word] = trans_err
-                
+# ----------------------------------------------------------------------------------------------------------------------------------------------------------
+                phon_edd, phon_count = phoneme_edit_distance(generated, target)
+                phon_err = phoneme_error(generated, target)
+
                 phon_edd_dict[word] = phon_edd
                 phon_count_dict[word] = phon_count
                 phon_err_dict[word] = phon_err
+# ----------------------------------------------------------------------------------------------------------------------------------------------------------
+                syll_edd, syll_count = syllable_edit_distance(generated, target)
+                syll_err = syllable_error(generated, target)
                 
                 syll_edd_dict[word] = syll_edd
                 syll_count_dict[word] = syll_count
                 syll_err_dict[word] = syll_err
+# ----------------------------------------------------------------------------------------------------------------------------------------------------------
+                stress_edd, stress_count = syllable_with_stress_edit_distance(generated, target)
+                stress_err = syllable_with_stress_error(generated, target)
 
-    trans_edits, trans_sizes, trans_errors, trans_counts = [], [], [], []
-    phon_edits, phon_sizes, phon_errors, phon_counts = [], [], [], []
-    syll_edits, syll_sizes, syll_errors, syll_counts = [], [], [], []
-    for lang in languages:
-        trans_edd = sum(lang_trans_err[lang].values())
-        trans_edits.append(trans_edd)
+                stress_edd_dict[word] = stress_edd
+                stress_count_dict[word] = stress_count
+                stress_err_dict[word] = stress_err
+# ----------------------------------------------------------------------------------------------------------------------------------------------------------
+                stress_edd, stress_count = syllable_with_stress_edit_distance(generated, target)
+                stress_err = syllable_with_stress_error(generated, target)
 
-        trans_size = sum(lang_trans_count[lang].values())
-        trans_sizes.append(trans_size)
-        
-        trans_err = sum(lang_trans_err[lang].values())
-        trans_errors.append(trans_err)
-        
-        trans_count = len(lang_trans_err[lang])
-        trans_counts.append(trans_count)
-        
-        phon_edd = sum(lang_phon_err[lang].values())
-        phon_edits.append(phon_edd)
+                stress_edd_dict[word] = stress_edd
+                stress_count_dict[word] = stress_count
+                stress_err_dict[word] = stress_err
+# ----------------------------------------------------------------------------------------------------------------------------------------------------------
+                pause_edd, pause_count = pause_edit_distance(generated, target)
+                pause_err = pause_error(generated, target)
 
-        phon_size = sum(lang_phon_count[lang].values())
-        phon_sizes.append(phon_size)
-        
-        phon_err = sum(lang_phon_err[lang].values())
-        phon_errors.append(phon_err)
-        
-        phon_count = len(lang_phon_err[lang])
-        phon_counts.append(phon_count)
-        
-        syll_edd = sum(lang_syll_err[lang].values())
-        syll_edits.append(syll_edd)
+                pause_edd_dict[word] = pause_edd
+                pause_count_dict[word] = pause_count
+                pause_err_dict[word] = pause_err
 
-        syll_size = sum(lang_syll_count[lang].values())
-        syll_sizes.append(syll_size)
-        
-        syll_err = sum(lang_syll_err[lang].values())
-        syll_errors.append(syll_err)
-        
-        syll_count = len(lang_syll_err[lang])
-        syll_counts.append(syll_count)
-        
-        trans_edits_rate = trans_edd / trans_size
-        trans_error_rate = trans_err / trans_count
-        phon_edits_rate = phon_edd / phon_size
-        phon_error_rate = phon_err / phon_count
-        syll_edits_rate = syll_edd / syll_size
-        syll_error_rate = syll_err / syll_count
-        
-        evaluation_result.setdefault(lang, {}).update({'trans_edits_rate': trans_edits_rate})
-        evaluation_result.setdefault(lang, {}).update({'trans_error_rate': trans_error_rate})
-        evaluation_result.setdefault(lang, {}).update({'phon_edits_rate': phon_edits_rate})
-        evaluation_result.setdefault(lang, {}).update({'phon_error_rate': phon_error_rate})
-        evaluation_result.setdefault(lang, {}).update({'syll_edits_rate': syll_edits_rate})
-        evaluation_result.setdefault(lang, {}).update({'syll_error_rate': syll_error_rate})
+    collect_evaluation_results(evaluation_result, languages, lang_trans_edd, lang_trans_count, lang_trans_err, 'trans')
+    collect_evaluation_results(evaluation_result, languages, lang_phon_edd, lang_phon_count, lang_phon_err, 'phon')
+    collect_evaluation_results(evaluation_result, languages, lang_syll_edd, lang_syll_count, lang_syll_err, 'syll')
+    collect_evaluation_results(evaluation_result, languages, lang_stress_edd, lang_stress_count, lang_stress_err, 'stress')
+    collect_evaluation_results(evaluation_result, languages, lang_pause_edd, lang_pause_count, lang_pause_err, 'pause')
     
-    mean_trans_edits_rate = sum(trans_edits) / sum(trans_sizes)
-    mean_trans_error_rate = sum(trans_errors) / sum(trans_counts)
-    mean_phon_edits_rate = sum(phon_edits) / sum(phon_sizes)
-    mean_phon_error_rate = sum(phon_errors) / sum(phon_counts)
-    mean_syll_edits_rate = sum(syll_edits) / sum(syll_sizes)
-    mean_syll_error_rate = sum(syll_errors) / sum(syll_counts)
-    
-    evaluation_result['mean_trans_edits_rate'] = mean_trans_edits_rate
-    evaluation_result['mean_trans_error_rate'] = mean_trans_error_rate
-    evaluation_result['mean_phon_edits_rate'] = mean_phon_edits_rate
-    evaluation_result['mean_phon_error_rate'] = mean_phon_error_rate
-    evaluation_result['mean_syll_edits_rate'] = mean_syll_edits_rate
-    evaluation_result['mean_syll_error_rate'] = mean_syll_error_rate
-
     return evaluation_result
--- dp/training/metrics.py
+++ dp/training/metrics.py
@@ -4,121 +4,177 @@ from typing import List, Union, Tuple
 
 
 def transcription_error(predicted: str, target: str) -> float:
-  return equal_strings(predicted=predicted, target=target)
+    return equal_strings(predicted=predicted, target=target)
 
 def transcription_edit_distance(predicted: str, target: str) -> Tuple[int, int]:
-  return edit_distance(predicted=predicted, target=target)
+    return edit_distance(predicted=predicted, target=target)
+
+def transcription_edit_distance_transposition(predicted: str, target: str) -> Tuple[int, int]:
+    return edit_distance_transposition(predicted=predicted, target=target)
 
 def phoneme_error(predicted: str, target: str) -> Tuple[int, int]:
-  pred_only_phoneme = remove_syllable_markers(predicted)
-  target_only_phoneme = remove_syllable_markers(target)
-  return equal_strings(predicted=pred_only_phoneme, target=target_only_phoneme)
+    pred_only_phoneme = remove_syllable_markers(predicted)
+    target_only_phoneme = remove_syllable_markers(target)
+    return equal_strings(predicted=pred_only_phoneme, target=target_only_phoneme)
 
 def phoneme_edit_distance(predicted: str, target: str) -> Tuple[int, int]:
-  pred_only_phoneme = remove_syllable_markers(predicted)
-  target_only_phoneme = remove_syllable_markers(target)
-  return edit_distance(predicted=pred_only_phoneme, target=target_only_phoneme)
+    pred_only_phoneme = remove_syllable_markers(predicted)
+    target_only_phoneme = remove_syllable_markers(target)
+    return edit_distance(predicted=pred_only_phoneme, target=target_only_phoneme)
+
+def phoneme_edit_distance_transposition(predicted: str, target: str) -> Tuple[int, int]:
+    pred_only_phoneme = remove_syllable_markers(predicted)
+    target_only_phoneme = remove_syllable_markers(target)
+    return edit_distance_transposition(predicted=pred_only_phoneme, target=target_only_phoneme)
 
 def syllable_error(predicted: str, target: str) -> float:
-  pred_syllables = remove_phonemes(standardize_syllable_markers(predicted))
-  target_syllables = remove_phonemes(standardize_syllable_markers(target))
-  return equal_strings(predicted=pred_syllables, target=target_syllables)
+    pred_syllables = remove_phonemes(standardize_syllable_markers(predicted))
+    target_syllables = remove_phonemes(standardize_syllable_markers(target))
+    return equal_strings(predicted=pred_syllables, target=target_syllables)
 
 def syllable_edit_distance(predicted: str, target: str) -> Tuple[int, int]:
-  pred_only_syllable = remove_phonemes(standardize_syllable_markers(predicted))
-  target_only_syllable = remove_phonemes(standardize_syllable_markers(target))
-  return edit_distance(predicted=pred_only_syllable, target=target_only_syllable)
+    pred_only_syllable = remove_phonemes(standardize_syllable_markers(predicted))
+    target_only_syllable = remove_phonemes(standardize_syllable_markers(target))
+    return edit_distance(predicted=pred_only_syllable, target=target_only_syllable)
+
+def syllable_edit_distance_transposition(predicted: str, target: str) -> Tuple[int, int]:
+    pred_only_syllable = remove_phonemes(standardize_syllable_markers(predicted))
+    target_only_syllable = remove_phonemes(standardize_syllable_markers(target))
+    return edit_distance_transposition(predicted=pred_only_syllable, target=target_only_syllable)
 
 def syllable_with_stress_error(predicted: str, target: str) -> float:
-  pred_syllable_with_stress = remove_phonemes(add_syllable_marker(predicted))
-  target_syllable_with_stress = remove_phonemes(add_syllable_marker(target))
-  return equal_strings(predicted=pred_syllable_with_stress, target=target_syllable_with_stress)
+    pred_syllable_with_stress = remove_phonemes(add_syllable_marker(predicted))
+    target_syllable_with_stress = remove_phonemes(add_syllable_marker(target))
+    return equal_strings(predicted=pred_syllable_with_stress, target=target_syllable_with_stress)
 
 def syllable_with_stress_edit_distance(predicted: str, target: str) -> Tuple[int, int]:
-  pred_syllable_with_stress = remove_phonemes(add_syllable_marker(predicted))
-  target_syllable_with_stress = remove_phonemes(add_syllable_marker(target))
-  return edit_distance(predicted=pred_syllable_with_stress, target=target_syllable_with_stress)
+    pred_syllable_with_stress = remove_phonemes(add_syllable_marker(predicted))
+    target_syllable_with_stress = remove_phonemes(add_syllable_marker(target))
+    return edit_distance(predicted=pred_syllable_with_stress, target=target_syllable_with_stress)
+
+def syllable_with_stress_edit_distance_transposition(predicted: str, target: str) -> Tuple[int, int]:
+    pred_syllable_with_stress = remove_phonemes(add_syllable_marker(predicted))
+    target_syllable_with_stress = remove_phonemes(add_syllable_marker(target))
+    return edit_distance_transposition(predicted=pred_syllable_with_stress, target=target_syllable_with_stress)
 
 def pause_error(predicted: str, target: str) -> float:
-  pred_pause = remove_phonemes(add_syllable_marker(predicted))
-  target_pause = remove_phonemes(add_syllable_marker(target))
-  return equal_strings(predicted=pred_pause, target=target_pause)
+    pred_pause = remove_phonemes(add_syllable_marker(predicted))
+    target_pause = remove_phonemes(add_syllable_marker(target))
+    return equal_strings(predicted=pred_pause, target=target_pause)
 
 def pause_edit_distance(predicted: str, target: str) -> Tuple[int, int]:
-  pred_pause = remove_phonemes(add_syllable_marker(predicted))
-  target_pause = remove_phonemes(add_syllable_marker(target))
-  return edit_distance(predicted=pred_pause, target=target_pause)
+    pred_pause = remove_phonemes(add_syllable_marker(predicted))
+    target_pause = remove_phonemes(add_syllable_marker(target))
+    return edit_distance(predicted=pred_pause, target=target_pause)
+
+def pause_edit_distance_transposition(predicted: str, target: str) -> Tuple[int, int]:
+    pred_pause = remove_phonemes(add_syllable_marker(predicted))
+    target_pause = remove_phonemes(add_syllable_marker(target))
+    return edit_distance_transposition(predicted=pred_pause, target=target_pause)
 
 def equal_strings(predicted: str, target: str) -> float:
-  """Calculates the word error rate of a single word result.
+    """Calculates the word error rate of a single word result.
 
-  Args:
-    predicted: Predicted word.
-    target: Target word.
-    predicted: List[Union[str: 
-    int]]: 
-    target: List[Union[str: 
+    Args:
+      predicted: Predicted word.
+      target: Target word.
+      predicted: List[Union[str: 
+      int]]: 
+      target: List[Union[str: 
 
-  Returns:
-    Word error
+    Returns:
+      Word error
 
-  """
+    """
 
-  return int(predicted != target)
+    return int(predicted != target)
+
+def edit_distance_transposition(predicted: str, target: str) -> Tuple[int, int]:
+    """Calculates the phoneme error rate of a single result based on the Levenshtein distance.
+
+    Args:
+      predicted: Predicted word.
+      target: Target word.
+      predicted: List[Union[str: 
+      int]]: 
+      target: List[Union[str: 
+
+    Returns:
+      Phoneme error.
+
+    """
+
+    d = numpy.zeros((len(target) + 1, len(predicted) + 1), dtype=numpy.uint8)
+    for i in range(len(target) + 1):
+        d[i][0] = i
+    for j in range(len(predicted) + 1):
+        d[0][j] = j
+
+    for i in range(1, len(target) + 1):
+        for j in range(1, len(predicted) + 1):
+            if target[i - 1] == predicted[j - 1]:
+                d[i][j] = d[i - 1][j - 1]
+            else:
+                substitution = d[i - 1][j - 1] + 1
+                insertion = d[i][j - 1] + 1
+                deletion = d[i - 1][j] + 1
+                d[i][j] = min(substitution, insertion, deletion)
+
+            if i > 1 and j > 1 and target[i-1] == predicted[j-2] and target[i-2] == predicted[j-1]:
+               d[i, j] = min(d[i, j], d[i-2, j-2] + 1)
+
+    return d[len(target)][len(predicted)], len(target)
 
 def edit_distance(predicted: str, target: str) -> Tuple[int, int]:
-  """Calculates the phoneme error rate of a single result based on the Levenshtein distance.
+    """Calculates the phoneme error rate of a single result based on the Levenshtein distance.
 
-  Args:
-    predicted: Predicted word.
-    target: Target word.
-    predicted: List[Union[str: 
-    int]]: 
-    target: List[Union[str: 
+    Args:
+      predicted: Predicted word.
+      target: Target word.
+      predicted: List[Union[str: 
+      int]]: 
+      target: List[Union[str: 
 
-  Returns:
-    Phoneme error.
+    Returns:
+      Phoneme error.
 
-  """
+    """
 
-  d = numpy.zeros((len(target) + 1, len(predicted) + 1), dtype=numpy.uint8)
-  for i in range(len(target) + 1):
-    d[i][0] = i
-  for j in range(len(predicted) + 1):
-    d[0][j] = j
+    d = numpy.zeros((len(target) + 1, len(predicted) + 1), dtype=numpy.uint8)
+    for i in range(len(target) + 1):
+        d[i][0] = i
+    for j in range(len(predicted) + 1):
+        d[0][j] = j
 
-  for i in range(1, len(target) + 1):
-    for j in range(1, len(predicted) + 1):
-      if target[i - 1] == predicted[j - 1]:
-        d[i][j] = d[i - 1][j - 1]
-      else:
-        substitution = d[i - 1][j - 1] + 1
-        insertion = d[i][j - 1] + 1
-        deletion = d[i - 1][j] + 1
-        d[i][j] = min(substitution, insertion, deletion)
+    for i in range(1, len(target) + 1):
+        for j in range(1, len(predicted) + 1):
+            if target[i - 1] == predicted[j - 1]:
+               d[i][j] = d[i - 1][j - 1]
+            else:
+                substitution = d[i - 1][j - 1] + 1
+                insertion = d[i][j - 1] + 1
+                deletion = d[i - 1][j] + 1
+                d[i][j] = min(substitution, insertion, deletion)
 
-      if i > 1 and j > 1 and target[i-1] == predicted[j-2] and target[i-2] == predicted[j-1]:
-        d[i, j] = min(d[i, j], d[i-2, j-2] + 1)
-
-  return d[len(target)][len(predicted)], len(target)
+    return d[len(target)][len(predicted)], len(target)
 
 def standardize_syllable_markers(string: str) -> Union[str,int]:
-  return add_syllable_marker(stress_marker_to_syllable_marker(string))
+    return add_syllable_marker(stress_marker_to_syllable_marker(string))
 
 def stress_marker_to_syllable_marker(string: str) -> Union[str,int]:
-  return re.sub(r"'", r".", string)
+    return re.sub(r"'", r".", string)
 
 def add_syllable_marker(string: str) -> Union[str,int]:
-  return re.sub(r"(^|\s)([^\s\.,'])", r"\1.\2", string)
+    return re.sub(r"(^|\s)([^\s\.,'])", r"\1.\2", string)
 
 def remove_phonemes(string: str) -> Union[str,int]:
-  return re.sub(r"[^\.'\s,]", '', string)
+    return re.sub(r"[^\.'\s,]", '', string)
 
 def remove_syllable_markers(string: str) -> Union[str,int]:
-  return re.sub(r"[\.']", '', string)
+    return re.sub(r"[\.']", '', string)
 
 def remove_pauses(string: str) -> Union[str,int]:
-  return re.sub(r"[\s,]", '', string)
+    return re.sub(r"[\s,]", '', string)
 
 def keep_pauses(string: str) -> Union[str,int]:
-  return remove_phonemes(remove_syllable_markers(string))
+    return remove_phonemes(remove_syllable_markers(string))
